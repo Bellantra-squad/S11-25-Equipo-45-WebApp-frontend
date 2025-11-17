3d8ee4a4f7c32760506683f2c0394792e8c291e3
jissykakin <sicaji22@gmail.com>
2025-11-17 16:42:01 +0530

    feat(header): enhance user header with notifications and profile options

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,36 +1,27 @@
 import type { RefineThemedLayoutHeaderProps } from "@refinedev/antd";
-import { useGetIdentity } from "@refinedev/core";
 import {
-  Layout as AntdLayout,
-  Avatar,
+  Layout as AntdLayout,  
   Space,
   Switch,
   theme,
-  Typography,
 } from "antd";
 import React, { useContext } from "react";
 import { ColorModeContext } from "../../contexts/color-mode";
+import { Notifications } from "../layout/Notifications";
+import { CurrentUser } from './CurrentUser';
 
-const { Text } = Typography;
 const { useToken } = theme;
-
-type IUser = {
-  id: number;
-  name: string;
-  avatar: string;
-};
 
 export const Header: React.FC<RefineThemedLayoutHeaderProps> = ({
   sticky = true,
 }) => {
   const { token } = useToken();
-  const { data: user } = useGetIdentity<IUser>();
   const { mode, setMode } = useContext(ColorModeContext);
 
   const headerStyles: React.CSSProperties = {
     backgroundColor: token.colorBgElevated,
     display: "flex",
-    justifyContent: "flex-end",
+    justifyContent: "flex-end",    
     alignItems: "center",
     padding: "0px 24px",
     height: "64px",
@@ -44,16 +35,16 @@ export const Header: React.FC<RefineThemedLayoutHeaderProps> = ({
 
   return (
     <AntdLayout.Header style={headerStyles}>
-      <Space>
+      <Space style={{ columnGap:"30px" }}>
         <Switch
           checkedChildren="🌛"
           unCheckedChildren="🔆"
           onChange={() => setMode(mode === "light" ? "dark" : "light")}
           defaultChecked={mode === "dark"}
         />
-        <Space style={{ marginLeft: "8px" }} size="middle">
-          {user?.name && <Text strong>{user.name}</Text>}
-          {user?.avatar && <Avatar src={user?.avatar} alt={user?.name} />}
+        <Space  style={{ columnGap:"25px" }} size="middle">
+         <Notifications />    
+         <CurrentUser />        
         </Space>
       </Space>
     </AntdLayout.Header>
