739a9cb7da41aa3a854af7481abbb74623750e8a
jissykakin <sicaji22@gmail.com>
2025-11-17 16:33:35 +0530

    feat(ui): created base configuration structure under src/refine/config

--- src/App.tsx
+++ src/App.tsx
@@ -3,47 +3,43 @@ import { Refine } from "@refinedev/core";
 import { RefineKbarProvider } from "@refinedev/kbar";
 import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
 import {
-
   useNotificationProvider,
 } from "@refinedev/antd";
-
 import routerProvider from "@refinedev/react-router";
 import "@refinedev/antd/dist/reset.css";
 import "antd/dist/reset.css";
-import { App as AntdApp } from "antd";
-
+import { App as AntdApp, ConfigProvider } from "antd";
 import AppRouter from "./router/AppRouter";
-
 import { ColorModeContextProvider } from "./contexts/color-mode";
-
-
 import { refineOptions } from "./refine/options";
 import { resources } from "./refine/resources";
 import { authProvider, dataProvider } from "./refine/providers";
-
+import { appTheme } from "./refine/config/theme";
 
 function App() {
   return (
     <BrowserRouter>
-      <RefineKbarProvider>
-        <ColorModeContextProvider>
-          <AntdApp>
-            <DevtoolsProvider>
-              <Refine
-                dataProvider={dataProvider}
-                authProvider={authProvider}
-                routerProvider={routerProvider}
-                notificationProvider={useNotificationProvider}
-                resources={resources}
-                options={refineOptions}
-              >
-                <AppRouter />
-                <DevtoolsPanel />
-              </Refine>
-            </DevtoolsProvider>
-          </AntdApp>
-        </ColorModeContextProvider>
-      </RefineKbarProvider>
+     <ConfigProvider theme={appTheme}>
+        <RefineKbarProvider>
+          <ColorModeContextProvider>
+            <AntdApp>
+              <DevtoolsProvider>              
+                <Refine
+                  dataProvider={dataProvider}
+                  authProvider={authProvider}
+                  routerProvider={routerProvider}
+                  notificationProvider={useNotificationProvider}
+                  resources={resources}
+                  options={refineOptions}
+                >                
+                  <AppRouter />
+                  <DevtoolsPanel />                
+                </Refine>             
+              </DevtoolsProvider>
+            </AntdApp>
+          </ColorModeContextProvider>
+        </RefineKbarProvider>
+      </ConfigProvider>
     </BrowserRouter>
   );
 }
